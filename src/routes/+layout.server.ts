af698e56a404aa4bcb8eb57fe845ac089cee0527
andras-szesztai <and.szesztai@gmai.com>
2023-03-04 19:01:58 +0100

    test: without auth

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,5 +1,6 @@
 import type { LayoutServerLoad } from './$types';
 
 export const load = (async ({ locals }) => ({
-	session: await locals.getSession()
+	// session: await locals.getSession()
+	session: null
 })) satisfies LayoutServerLoad;
