93861f440575bbd53cdb296a7906f7df3cf9eacb
andras-szesztai <and.szesztai@gmai.com>
2023-03-04 18:56:15 +0100

    fix: yml & env

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -21,6 +21,7 @@ const authorization = (async ({ event, resolve }) => {
 
 export const handle: Handle = sequence(
 	SvelteKitAuth({
+		trustHost: true,
 		// @ts-ignore
 		providers: [GitHub({ clientId: SECRET_GITHUB_ID, clientSecret: SECRET_GITHUB_SECRET })]
 	}),
